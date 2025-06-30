7cad728494922d8f5cebdfc41e93157b2f57f1d3
elroi_133 <roissmerlanza@gmail.com>
2025-06-30 09:39:29 -0400

    mejoras en la vista, y agregando el estatus de la bateria

--- scripts/update_wttr.sh
+++ scripts/update_wttr.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Definir manualmente HOME si no existe
+if [ -z "$HOME" ]; then
+  export HOME=$(getent passwd "$(id -un)" | cut -d: -f6)
+fi
+
+# Asegurar que exista la ruta ~/.cache
+mkdir -p "$HOME/.cache"
+
+# Ruta de salida
 OUTPUT_FILE="$HOME/.cache/wttr_info.txt"
 
-DATA=$(curl -s "wttr.in/~Barcelona+venezuela?format=Temp:%t+Hume:%h+Vien:%w+\\nLluvia:%p+Luna:%m")
+# Asegura que el directorio exista
+mkdir -p "$(dirname "$OUTPUT_FILE")"
 
+# Descarga y analiza la info del clima
+DATA=$(curl -s "wttr.in/~Barcelona+venezuela?format=Temp:%t+Hume:%h+Vien:%w+\\nLluvia:%p+Luna:%m")
 ICON=$(echo "$DATA" | grep -o "Luna:.*" | cut -d':' -f2)
 
 case "$ICON" in
@@ -19,4 +31,5 @@ case "$ICON" in
 esac
 
 FORMATTED=$(echo "$DATA" | sed "s/Luna:$ICON/Luna: $PHASE/")
+
 echo -e "$FORMATTED" > "$OUTPUT_FILE"
